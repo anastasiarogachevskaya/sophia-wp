c62d5e9456782db134853d86bde57537d7c4d8fc
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-10-01 12:43:36 +0300

    huge background pic

--- themes/sophia-landing/front-page.php
+++ themes/sophia-landing/front-page.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-<h2>Hello World</h2>
+<div class="background-wrapper">
+    <div class="centered-content">
+        <p>
+            Follow @sofiaruutu on 
+            <a href="https://instagram.com/sofiaruutu" target="_blank">Instagram</a>, 
+            <a href="https://tiktok.com/@sofiaruutu" target="_blank">TikTok</a>, and 
+            <a href="https://youtube.com/user/sofiaruutu" target="_blank">YouTube</a>.
+        </p>
+    </div>
+</div>
+
 <?php get_footer(); ?>
