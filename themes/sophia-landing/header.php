5140a3fb2511043d6facf3b0a26bdc1550a27733
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-10-01 19:10:59 +0300

    Final touches

--- themes/sophia-landing/header.php
+++ themes/sophia-landing/header.php
@@ -5,6 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/wp-content/themes/sophia-landing/assets/favicon.png" sizes="32x32"/>
+    <link rel="icon" href="/wp-content/themes/sophia-landing/assets/favicon.png" sizes="192x192"/>
+    <link rel="apple-touch-icon" href="/wp-content/themes/sophia-landing/assets/favicon.png" />
+    <meta name="msapplication-TileImage" content="/wp-content/themes/sophia-landing/assets/favicon.png" />
+    <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }
+                ,
+                i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            }
+            )(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            ga('create', 'UA-55297610-1', 'auto');
+            ga('send', 'pageview');
+        </script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
