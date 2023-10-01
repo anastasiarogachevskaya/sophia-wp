d26db99d87826cf3b2d5e12054cddac427441446
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-10-01 12:34:33 +0300

    vite basics

--- themes/sophia-landing/functions.php
+++ themes/sophia-landing/functions.php
@@ -6,4 +6,24 @@ function redirect_all_to_front_page() {
     }
 }
 add_action('template_redirect', 'redirect_all_to_front_page');
+
+function sophia_landing_enqueue_scripts() {
+    // Check if we're in development mode
+    // For testing purposes, always enqueue from Vite's server
+    wp_enqueue_script('sophia-landing-main-js', 'http://localhost:5173/assets/main.js', array(), null, true);
+    wp_enqueue_style('sophia-landing-main-css', 'http://localhost:5173/assets/main.scss', array(), null);
+
+    // if (defined('WP_DEBUG') && WP_DEBUG) {
+    //     // Enqueue from Vite's server
+    //     wp_enqueue_script('sophia-landing-main-js', 'http://127.0.0.1:5173/main.js', array(), null, true);
+    //     wp_enqueue_style('sophia-landing-main-css', 'http://127.0.0.1:5173/style.css', array(), null);
+    // } else {
+    //     // Enqueue production files
+    //     wp_enqueue_script('sophia-landing-main-js', get_template_directory_uri() . '/dist/main.js', array(), null, true);
+    //     wp_enqueue_style('sophia-landing-main-css', get_template_directory_uri() . '/dist/style.css', array(), null);
+    // }
+}
+add_action('wp_enqueue_scripts', 'sophia_landing_enqueue_scripts');
+
+
 ?>
--- themes/sophia-landing/vite.config.js
+++ themes/sophia-landing/vite.config.js
@@ -1,5 +1,8 @@
 // vite.config.js
+import ViteSass from 'vite-plugin-sass';
+
 export default {
+  plugins: [ViteSass()],
   build: {
     rollupOptions: {
       input: './assets/main.js', // Replace with the correct path
